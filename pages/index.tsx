a51ddd407d39d086f15fa6e283eddc1af94363a5
Levan Katsadze <leodevbro@gmail.com>
2023-02-12 14:46:13 +0400

    basic styling setup step2

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,16 +10,24 @@ const styles = {
   ],
 }
 
-const IndexPage = () => (
-  <div css={[styles.container({ hasBackground: true })]}>
-    <div tw="flex flex-col justify-center h-full gap-y-5 md:accent-amber-50">
-      <Button variant="primary">Submit</Button>
-      <Button variant="secondary">Cancel</Button>
-      <Button isSmall>Close</Button>
+const sampleComp = () => {
+  return (
+    <div className="ooppp" css={[styles.container({ hasBackground: true })]}>
+      <div tw="flex flex-col justify-center h-full gap-y-5 md:accent-amber-50">
+        <Button style={{ marginBottom: '900px' }} variant="primary">
+          Submit
+        </Button>
+        <Button variant="secondary">Cancel</Button>
+        <Button isSmall>Close</Button>
+      </div>
+      <Logo />
+      <p style={{ fontSize: '1rem' }}>444444444</p>
     </div>
-    <Logo />
-    <p style={{ fontSize: '1rem' }}>444444444</p>
-  </div>
-)
+  )
+}
+
+const IndexPage = () => {
+  return <div>ka</div>
+}
 
 export default IndexPage
